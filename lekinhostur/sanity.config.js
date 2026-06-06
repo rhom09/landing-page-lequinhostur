520141b52a5f005cc5ee69f68adcd688919c0361
Rhom09 <rhom0909@gmail.com>
2026-06-05 22:07:22 -0300

    feat: update Sanity plugins configuration to include structured content listing

--- lekinhostur/sanity.config.js
+++ lekinhostur/sanity.config.js
@@ -10,7 +10,18 @@ export default defineConfig({
   projectId: 'zv6ynzi7',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({
+    structure: (S) =>
+      S.list()
+        .title('Conteúdo')
+        .items([
+          S.documentTypeListItem('excursao').title('Excursões'),
+          S.documentTypeListItem('destino').title('Destinos'),
+          S.documentTypeListItem('proximaSaida').title('Próximas Saídas'),
+          S.documentTypeListItem('estatistica').title('Estatísticas'),
+          S.documentTypeListItem('depoimento').title('Depoimentos'),
+        ]),
+  }), visionTool()],
 
   schema: {
     types: schemaTypes,
